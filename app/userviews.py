da22ba2ec803d04dd4d75fe343b12eca30ba5d36
skrish-n <080sai@gmail.com>
2021-09-08 16:55:30 +0530

    Last Blr commit

--- app/userviews.py
+++ app/userviews.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-from flask import render_template, make_response, flash, url_for
+from flask import render_template, make_response, flash, url_for, request
 from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required,
                                 get_jwt_identity, get_raw_jwt)
 from .models import User, UserStock, RevokedToken
@@ -18,32 +18,34 @@ class UserRegistration(Resource):
     def post(self):
         print("#######Entering UserRegistration#######")
         import email_sends, generic_technical
-        data = parser.parse_args()
-        fetch_user = User.objects(username=data['username'])
+        #data = parser.parse_args()
+        fetch_user = User.objects(username=request.form['username'])
         if fetch_user:
-            return {'message': 'User {} already exists'.format(data['username'])}
+            #return {'message': 'User {} already exists'.format(request.form['username'])}
+            return make_response(render_template('register.html', user_exist= 'yes',user_name=request.form['username'],headers = {'Content-Type': 'text/html'}))
         try:
 
-            new_user = User(username=data['username'], password=generic_technical.generate_hash(data['password']),
-                            firstname=data['firstname'], lastname=data['lastname']
+            new_user = User(username=request.form['username'], password=generic_technical.generate_hash(request.form['password']),
+                            firstname=request.form['firstname'], lastname=request.form['lastname']
                             , activated=False, updated_at=datetime.now(), created_at=datetime.now())
             new_user.save()
 
-            registration_token = generic_technical.generate_confirmation_token(data['username'])
+            registration_token = generic_technical.generate_confirmation_token(request.form['username'])
             print('registration token:', registration_token)
             confirm_url = url_for("api.confirm", token=registration_token, _external=True)
             print(confirm_url)
             html = render_template('activate.html', confirm_url=confirm_url)
             subject = "Please confirm your email"
-            email_sends.send_email(data['username'], subject, html)
+            email_sends.send_email(request.form['username'], subject, html)
 
             print("#######Exiting UserRegistration-Success#######")
             return {
-                'message': 'User {} was created'.format(data['username']),
+                'message': 'User {} was created'.format(request.form['username']),
             }
         except:
             print("#######Exiting UserRegistration-Fail#######")
-            return {'message': 'Something went wrong'}, 500
+            #return {'message': 'Something went wrong'}, 500
+            return
 
     def get(self):
         headers = {'Content-Type': 'text/html'}
@@ -57,7 +59,10 @@ class UserLogin(Resource):
         current_user = User.objects(username=data['username']).first()
         print(current_user)
         if current_user is None:
-            return {'message': 'User {} does not exist'.format(data['username'])}
+            #return {'message': 'User {} does not exist'.format(data['username'])}
+            return make_response(render_template('login.html', user_exist='nope',headers = {'Content-Type': 'text/html'}))
+        if not current_user['activated']:
+            return make_response(render_template('login.html', acc_activated='nope',headers = {'Content-Type': 'text/html'}))
 
         if generic_technical.verify_hash(data['password'], current_user['password']):
             access_token = create_access_token(identity=data['username'])
@@ -74,7 +79,6 @@ class UserLogin(Resource):
 class UserLogoutAccess(Resource):
     @jwt_required
     def post(self):
-        import generic_technical
         # token_data = TokenCollection(jti=get_raw_jwt()['jti'])
         jti = get_raw_jwt()['jti']
 
